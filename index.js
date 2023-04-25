c6a4165be8eb74c1aefec7a98d469e8ef7ace90b
Medetcan Diler <medetdiler1996@gmail.com>
2023-04-25 21:45:56 +0300

    Completed assignmnet

--- index.js
+++ index.js
@@ -1,10 +1,44 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+// function isPalindrome(word) {
+//   // Write your algorithm here
+//   let backedWord = backWord(word);
+//   if( word === backedWord){
+//     return true;
+//   }else {
+//     return false;
+//   }
+// }
+
+// const backWord = word => word.split('').reverse().join('');
+
+function isPalindrome(word){
+  for( let i = 0; i < word.length/2; i++){
+    if( word[i] === word[(word.length - 1) - i]){
+      return true
+    } else {
+      return false;
+    }
+  }
 }
+
+isPalindrome('abba')
+
 
 /* 
   Add your pseudocode here
+  ---
+  reverse the word backword 
+  if the reversed word equal to initial word return true 
+  else return false ;
 */
+
+/*
+  Add your pseucode here
+  ---
+  if the first letter same as last letter, and he second is the same as second to last second, until we reach the middle. return true
+  otherwise return false
+
+*/ 
+
 
 /*
   Add written explanation of your solution here
